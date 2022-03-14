d06110f8b5d79c0507a068a08579bad1ddf132f0
hkjang <gagagiga@naver.com>
2022-03-14 11:53:40 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+node-red-contrib-googleplayscraper
+================
+
+Node-RED node for google-play-scraper
+
+
+
+## Install
+
+To install the stable version use the `Menu - Manage palette - Install`
+option and search for node-red-contrib-googleplayscraper, or run the following
+command in your Node-RED user directory, typically `~/.node-red`
+
+    npm install node-red-contrib-googleplayscraper
+
+## wrapper googleplayscraper
+Node.js module to scrape application data from the Google Play store
+- https://github.com/facundoolano/google-play-scraper
+
+
---
+++ node.js
@@ -0,0 +1,33 @@
+const gplay  = require('google-play-scraper');
+module.exports = function (RED) {
+    function FunctionNode(n) {
+        RED.nodes.createNode(this, n);
+
+        var node = this;
+        this.name = n.name;
+        for (var key in n) {
+            node[key] = n[key] || "";
+        }
+
+        this.on('input', function (msg) {
+            for (var i in msg) {
+                if (i !== 'req' | i !== 'res' | i !== 'payload' | i !== 'send' | i !== '_msgid') {
+                    node[i] = node[i] || msg[i];
+                }
+            }
+            try{
+                node.error(node.appId);
+                gplay.app({appId: node.appId})
+                    .then(res);
+                function res(results){
+                    msg.payload = results;
+                    node.send(msg);
+                }
+            }catch (e){
+                node.error(e);
+            }
+        });
+    }
+    RED.nodes.registerType("googleplayscraper", FunctionNode, {});
+};
+
